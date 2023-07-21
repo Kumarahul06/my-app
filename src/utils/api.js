993c6a9a8e9d6f3a786463f4f4ffe4be446c05d1
Kumarahul06 <kumarahul1321@gmail.com>
2023-07-22 05:05:40 +0530

    my app

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 
 
-const BASE_URL = "https://youtube138.p.rapidapi.com/auto-complete/";
+const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
-     params: { hl: "en", gl: "US" },
+     params: { 
+      hl: "en",
+       gl: "US" 
+      },
     headers: {
-      "X-RapidAPI-Key": 
-      process.env.REACT_APP_YOUTUBE_APP_KEY,
+      "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_APP_KEY,
       "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
   };
 
   export const fetchDataFromApi = async (url) => {
-    const {data} = await axios.get(`${BASE_URL}/${url}`, options);
+    const {data} = await axios.get(`${BASE_URL}/${url}`,options);
     return data;
   };
